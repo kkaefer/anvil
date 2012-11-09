6c4752bb92440b02933238d2870c73ecf9a8b65c
Konstantin Käfer <github@kkaefer.com>
2012-11-09 10:45:26 +0100

    rename filters to transforms and restructure

--- forge.js
+++ forge.js
@@ -2,13 +2,38 @@
 var async = require('async');
 var fs = require('fs');
 var path = require('path');
-var Route = require('./lib/route');
+var _ = require('underscore');
+
+
 var Item = require('./lib/item');
+var Route = require('./lib/route');
+var Router = require('./lib/router');
+var Tree = require('./lib/tree');
+
+require('./lib/tools');
 
 module.exports = exports = Forge;
 function Forge() {
-    this._setup();
+    _.bindAll(this);
+
+    this.base = ''; // base path
+    this.input = 'content';
+    this.output = 'output';
+
+    this.templates = {};
+    this.pendingItems = [];
+    this.items = _([]);
+    this.routes = [];
+    this.router = new Router(this.routes);
+    this.generators = [];
 }
+
+// --- static ---
+
+Forge.filter = require('./lib/filter');
+Forge.transform = require('./lib/transform');
+
+// --- public ---
 
 Forge.prototype.route = function(path) {
     for (var i = 1; i < arguments.length; i++) {
@@ -61,8 +86,60 @@ Forge.prototype.compile = function() {
     ], this._report);
 };
 
-// === private ===
-require('./lib/forge');
-require('./lib/filters');
-require('./lib/tools');
+// --- private ---
 
+Forge.prototype._traverse = function(done) {
+    var app = this;
+    this.tree = new Tree(this.input);
+    this.tree.traverse(function(source, stat, done) {
+        var route = source.substring(this.root.length);
+        var item = new Item({ route: route, source: source, stat: stat });
+        app.pendingItems.push(item);
+        done();
+    }, done);
+};
+
+Forge.prototype._dispatch = function(done) {
+    var app = this;
+
+    function iterate() {
+        if (!app.pendingItems.length) return done();
+        var item = app.pendingItems.shift();
+
+        item.load(function(err) {
+            if (err) item.reportError(err);
+            app.router.dispatch(item, function(err) {
+                if (err) item.reportError(err);
+                else if (!item.ignore) {
+                    app.items.push(item);
+                }
+                iterate();
+            });
+        });
+    }
+
+    iterate(null);
+};
+
+Forge.prototype._generate = function(done) {
+    async.forEachSeries(this.generators, function(generator, next) {
+        if (generator.length >= 1) {
+            generator(next);
+        } else {
+            generator();
+            next();
+        }
+    }, done);
+};
+
+Forge.prototype._write = function(done) {
+    var app = this;
+    async.forEachSeries(app.items.value(), function(item, next) {
+        item.write(app, next);
+    }, done);
+};
+
+
+Forge.prototype._report = function(err) {
+    console.warn('Completed in %dms', Date.now() - this.started);
+};
